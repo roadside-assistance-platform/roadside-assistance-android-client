00ea5304e7c30aec2f485795389a43b7261a6d0f
Younes Bouhouche <younes.bouhouche12@gmail.com>
2025-02-06 17:54:31 +0100

    App initial setup

---
+++ app/src/main/java/esi/roadside/assistance/client/App.kt
@@ -0,0 +1,16 @@
+package esi.roadside.assistance.client
+
+import android.app.Application
+import esi.roadside.assistance.client.core.di.appModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
+
+class App: Application() {
+    override fun onCreate() {
+        super.onCreate()
+        startKoin {
+            androidContext(this@App)
+            modules(appModule)
+        }
+    }
+}
