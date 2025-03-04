03c970b4b0d7c8cbc60be781ac956a26c5210204
Younes Bouhouche <younes.bouhouche12@gmail.com>
2025-03-04 11:00:14 +0100

    Added Necessary Modules

--- app/src/main/java/esi/roadside/assistance/client/App.kt
+++ app/src/main/java/esi/roadside/assistance/client/App.kt
@@ -3,6 +3,7 @@ package esi.roadside.assistance.client
 import android.app.Application
 import esi.roadside.assistance.client.auth.di.authModule
 import esi.roadside.assistance.client.core.di.coreModule
+import esi.roadside.assistance.client.main.di.mainModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
 
@@ -11,7 +12,7 @@ class App: Application() {
         super.onCreate()
         startKoin {
             androidContext(this@App)
-            modules(coreModule, authModule)
+            modules(coreModule, authModule, mainModule)
         }
     }
 }
