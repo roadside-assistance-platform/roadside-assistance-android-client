f7a63d48405d4dbf6b4bc377551bc39d55725863
Younes Bouhouche <younes.bouhouche12@gmail.com>
2025-03-04 11:00:28 +0100

    Added Main screen actions

---
+++ app/src/main/java/esi/roadside/assistance/client/main/presentation/Action.kt
@@ -0,0 +1,8 @@
+package esi.roadside.assistance.client.main.presentation
+
+import com.mapbox.geojson.Point
+
+sealed interface Action {
+        data object RequestService: Action
+        data class SetLocation(val location: Point?): Action
+}
