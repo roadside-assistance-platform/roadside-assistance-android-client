eaa5baf327e21887dcbdc7295795f124070c1a0c
Younes Bouhouche <younes.bouhouche12@gmail.com>
2025-03-04 10:58:37 +0100

    Created Empty Profile Screen

---
+++ app/src/main/java/esi/roadside/assistance/client/main/presentation/routes/profile/ProfileScreen.kt
@@ -0,0 +1,9 @@
+package esi.roadside.assistance.client.main.presentation.routes.profile
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun ProfileScreen(modifier: Modifier = Modifier) {
+
+}
