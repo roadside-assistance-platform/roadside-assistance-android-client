4767882d889bebf6a71991eb2a4d44c1fa597606
ooooo7777 <oussamachatri22@gmail.com>
2025-03-04 10:49:37 +0100

    Add RequestAssistance screen

---
+++ app/src/main/java/esi/roadside/assistance/client/main/presentation/screens/HomePage.kt
@@ -0,0 +1,20 @@
+package esi.roadside.assistance.client.main.presentation.screens
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import esi.roadside.assistance.client.core.presentation.theme.PreviewAppTheme
+
+@Composable
+fun HomePage(modifier: Modifier = Modifier) {
+
+}
+
+
+@Preview
+@Composable
+private fun Preview() {
+    PreviewAppTheme {
+        HomePage()
+    }
+}
