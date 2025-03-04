57d0cc1a5603ddf834d77c1d7f5de0f28b310f25
Younes Bouhouche <younes.bouhouche12@gmail.com>
2025-03-04 10:59:32 +0100

    Added Main View Model

---
+++ app/src/main/java/esi/roadside/assistance/client/main/di/ViewModelModule.kt
@@ -0,0 +1,9 @@
+package esi.roadside.assistance.client.main.di
+
+import esi.roadside.assistance.client.main.presentation.MainViewModel
+import org.koin.core.module.dsl.viewModel
+import org.koin.dsl.module
+
+val viewModelModule = module {
+    viewModel { MainViewModel() }
+}
