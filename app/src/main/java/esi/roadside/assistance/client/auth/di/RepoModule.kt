21f936dd9fb1255285a4cc3b59b67e0d02247d42
Younes Bouhouche <younes.bouhouche12@gmail.com>
2025-03-07 09:43:05 +0100

    Fixed Cloudinary MediaManager injection

--- app/src/main/java/esi/roadside/assistance/client/auth/di/RepoModule.kt
+++ app/src/main/java/esi/roadside/assistance/client/auth/di/RepoModule.kt
@@ -8,16 +8,18 @@ import esi.roadside.assistance.client.auth.domain.repository.CloudinaryRepo
 import esi.roadside.assistance.client.core.data.networking.HttpClientFactory
 import io.ktor.client.engine.cio.CIO
 import org.koin.android.ext.koin.androidApplication
+import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
 
 val RepoModule = module {
     single { HttpClientFactory.create(CIO.create()) }
-    single {
+    single<MediaManager> {
         var config: HashMap<String, String> = HashMap()
         config.put("cloud_name", "dsvlwomdq")
         config.put("api_key", "996654622953693")
         config.put("api_secret", "d8gJrtFaBOSAkSrpaPNU4V4M2-Y")
-        MediaManager.init(androidApplication(), config)
+        MediaManager.init(androidContext(), config)
+        MediaManager.get()
     }
     single<AuthRepo> { AuthRepoImpl(get()) }
     single<CloudinaryRepo> { CloudinaryRepoImpl(get()) }
