29a4f6f245e7bb9ddb6133e8c8d71e6e446c9611
Younes Bouhouche <younes.bouhouche12@gmail.com>
2025-05-24 13:20:35 +0100

    [Refactor] Add Change Password ViewModel and localize strings for multiple languages

--- app/src/main/java/esi/roadside/assistance/client/main/di/ViewModelModule.kt
+++ app/src/main/java/esi/roadside/assistance/client/main/di/ViewModelModule.kt
@@ -4,13 +4,16 @@ import esi.roadside.assistance.client.main.presentation.MainViewModel
 import esi.roadside.assistance.client.main.presentation.routes.home.SearchViewModel
 import esi.roadside.assistance.client.main.presentation.routes.home.request.AssistanceViewModel
 import esi.roadside.assistance.client.main.presentation.routes.profile.ProfileViewModel
-import org.koin.android.ext.koin.androidContext
+import esi.roadside.assistance.client.main.presentation.routes.settings.ChangePasswordViewModel
 import org.koin.core.module.dsl.viewModel
 import org.koin.dsl.module
 
 val viewModelModule = module {
     viewModel {
         MainViewModel(get(), get(), get(), get(), get())
+    }
+    viewModel {
+        ChangePasswordViewModel(get(), get())
     }
     viewModel {
         AssistanceViewModel(get(), get())
